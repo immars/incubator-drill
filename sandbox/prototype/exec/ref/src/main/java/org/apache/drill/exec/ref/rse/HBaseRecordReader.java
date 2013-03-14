f70a190bdec1da87528d3e772a356b4342be5527
Snake <snake.fly@hotmail.com>
2013-03-14 14:10:32 +0800

    fix bug of hbase scanner

--- sandbox/prototype/exec/ref/src/main/java/org/apache/drill/exec/ref/rse/HBaseRecordReader.java
+++ sandbox/prototype/exec/ref/src/main/java/org/apache/drill/exec/ref/rse/HBaseRecordReader.java
@@ -117,20 +117,35 @@ public class HBaseRecordReader implements RecordReader {
                         currentScannerIndex++;
                         if (currentScannerIndex > scanners.size()-1) {
                             /* Already reached the last one */
-                            System.out.println("No more scanner left...");
                             LOG.info("No more scanner left...");
                             return NextOutcome.NONE_LEFT;
 
                         } else {
-                            System.out.println("Get data from next scanner...");
                             LOG.info("Get data from next scanner...");
                             TableScanner scanner = scanners.get(currentScannerIndex);
                             curRes.clear();
                             valIndex = 0;
                             hasMore = scanner.next(curRes);
+
+                            while (curRes.size() == 0) {
+                                /* Get next scanner with actual values */
+                                currentScannerIndex++;
+                                if (currentScannerIndex > scanners.size()-1) {
+                                    /* Already reached the last one */
+                                    LOG.info("No more scanner left...");
+                                    return NextOutcome.NONE_LEFT;
+                                } else {
+                                    LOG.info("Get data from next scanner...");
+                                    scanner = scanners.get(currentScannerIndex);
+                                    curRes.clear();
+                                    valIndex = 0;
+                                    hasMore = scanner.next(curRes);
+                                }
+                            }
                         }
                     }
                 }
+
                 KeyValue kv = curRes.get(valIndex++);
                 record.setClearAndSetRoot(rootPath, convert(kv));
                 return NextOutcome.INCREMENTED_SCHEMA_CHANGED;
